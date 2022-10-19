ff78854e0a53018f16be5c6b5d4b2a3afa643abb
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-10-20 00:17:20 +0600

    news id and cate id wise data shows api

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 app.use(cors())
 const categories = require('./data/categories.json');
+const news = require('./data/news.json')
 app.get('/', (req, res) => {
     res.send("hello world express")
 })
 app.get("/news-category", (req, res) => {
     res.send(categories);
 })
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const cats = news.filter(n => n.category_id === id);
+    res.send(cats)
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(news => news._id === id)
+    res.send(selectedNews)
+})
+
+
+
+
 app.listen(port, () => {
     console.log(`the server is running in port ${port}`);
 })
